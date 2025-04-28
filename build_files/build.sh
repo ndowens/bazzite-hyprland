00443f0204de2744876d1d1f7f4c6a10bb41c681
Jeremy Hage <jhage.codingkr@gmail.com>
2025-04-28 13:53:27 -0500

    revert disabling copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,7 +25,7 @@ enable_copr solopasha/hyprland
 enable_copr erikreider/SwayNotificationCenter
 enable_copr pgdev/ghostty
 
-dnf5 install -y \
+dnf5 install -y --setopt=install_weak_deps=False \
     xdg-desktop-portal-hyprland \
     hyprland \
     hyprlock \
@@ -50,9 +50,9 @@ dnf5 install -y \
     ghostty
 
 # Disable COPRs so they don't end up enabled on the final image:
-dnf5 -y copr disable solopasha/hyprland
-dnf5 -y copr disable erikreider/SwayNotificationCenter
-dnf5 -y copr disable pgdev/ghostty
+# dnf5 -y copr disable solopasha/hyprland
+# dnf5 -y copr disable erikreider/SwayNotificationCenter
+# dnf5 -y copr disable pgdev/ghostty
 
 #### Example for enabling a System Unit File
 
